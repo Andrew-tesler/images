ab98417190429c0d59033ffd318e7eeb888e8017
Andrew Tesler <tesler.andrew@gmail.com>
2014-06-27 12:02:58 +0300

    Added content function

--- start.sh
+++ start.sh
@@ -6,13 +6,24 @@
 # for choosing image
 #-------------------------------------------
 
+
+declare -a ARRAYNAME
+
+# Print on the terminal: Message to print Status to print (OK, INFO, ERROR)
 function print() {
     message=$1
     status=$2
     source ./message.sh $message $status
 }
 
+# Save all the content of the given folder to an array.
+function content() {
+    folder=$1
+    echo "folder: $folder"
+
+}
 
 print test INFO
 
-
+content "/tmp"
+echo $ARRAYNAME
