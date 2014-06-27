eef97b76dc7b426dd52ae9b33dc5d4858c3384b3
Andrew Tesler <tesler.andrew@gmail.com>
2014-06-27 11:49:06 +0300

    Added message.sh

---
+++ message.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Recive message and status (OK,ERROR,INFO) and print in on the console
+
+message=$1
+status=$2
+infoMessage=$3
+
+#echo -ne "$message \t\t " 
+
+    case $status in
+
+	OK)
+	   
+	    Message="\e[39m$message"
+	    status="\e[92m$status"
+	    ;;
+	ERROR)
+	    Message="\e[39m$message"
+	    status="\e[91m$status"
+	    ;;
+	INFO)
+	    Message="\e[39m$message"
+	    status="\e[93m$status"
+	    ;;
+	*)
+	    Message="Somthing went wrong with Message script. ????"
+	    ;;
+    esac
+
+    echo -ne "[$status\e[0m]\t"
+    
+    echo -en "$Message\t\e[0m"
+    
+    echo -e "$infoMessage"
+  #  echo -e "$status\e[0m"
