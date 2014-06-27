85dd83f8c51a86bb70d66c77edbaf2d93472058a
Andrew Tesler <tesler.andrew@gmail.com>
2014-06-27 11:48:29 +0300

    Added start.sh. initial commit

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#------------------------------------------
+# andrey.tesler@compulab.co.il
+# Script for burning images to usb with gui
+# for choosing image
+#-------------------------------------------
+
